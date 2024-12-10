33664c0ae9b93fcb475cc9d7a9d2760da9020711
Gabriel Isaac <stewarti3@yahoo.com>
2024-12-10 16:01:33 +0100

    fix api

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 import express from 'express';
 import routes from './routes/index';
 
-require('dotenv').config();
-
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -10,7 +8,7 @@ app.use(express.json());
 app.use('/', routes);
 
 app.listen(port, () => {
-  console.log(`Server running on PORT: ${port}`);
+    console.log(`Server running on port ${port}`);
 });
 
 export default app;
