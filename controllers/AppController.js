fcc95fe45f1c33fad4ba91b28213e4b3f78b7163
Femi Mehalayese <megafemclick@gmail.com>
2024-12-10 13:36:12 +0100

    appconter and router

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,1 +1,21 @@
-// app controller
+import db from '../utils/db';
+import redis from '../utils/redis';
+
+const express = require('express');
+
+const app = express();
+const port = 5000;
+
+app.get('/status', (req, res) => {
+  if (db.isAlive() && redis.isAlive()) {
+    res.statusCode(200).send('{ "redis": true, "db": true }');
+  }
+});
+
+app.get('/stats', (req, res) => {
+  const numFile = db.nbFiles();
+  const numUser = db.nbUsers();
+  res.statusCode(200).send(`{ "user": ${numUser}, "files": ${numFile} }`);
+});
+
+app.listen(port);
---
+++ server.js
@@ -0,0 +1,5 @@
+const express = require('express');
+const routes = require('./routes/index');
+
+const app = express();
+const port = 5000;
