992052295a5b9b380afefc71f56b220f465cc454
Gabriel Isaac <stewarti3@yahoo.com>
2024-12-10 17:04:42 +0100

    authenticate user

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,4 +1,5 @@
 import sha1 from 'sha1';
+import redisClient from '../utils/redis';
 import dbClient from '../utils/db';
 
 class UsersController {
@@ -27,6 +28,28 @@ class UsersController {
     const result = await dbClient.db.collection('users').insertOne(newUser);
 
     return res.status(201).json({ id: result.insertedId, email });
+  }
+
+  static async getMe(req, res) {
+    const token = req.headers['x-token'];
+    if (!token) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    const key = `auth_${token}`;
+    const userId = await redisClient.get(key);
+
+    if (!userId) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    const user = await dbClient.db.collection('users').findOne({ _id: new dbClient.ObjectID(userId) });
+
+    if (!user) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    return res.status(200).json({ id: user._id, email: user.email });
   }
 }
 
