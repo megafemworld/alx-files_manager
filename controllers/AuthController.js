940b2d05435d9a71bcedfdb7127e7b50f51d5ee4
Femi Mehalayese <megafemclick@gmail.com>
2024-12-11 16:45:37 +0100

    token key removed

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -23,9 +23,8 @@ class AuthController {
 
     const token = uuidv4();
     const key = `auth_${token}`;
-    const returnToken = `token: ${token}`;
     await redisClient.set(key, user._id.toString(), 86400);
-    return res.status(200).json({ returnToken });
+    return res.status(200).json({ token });
   }
 
   static async getDisconnect(req, res) {
